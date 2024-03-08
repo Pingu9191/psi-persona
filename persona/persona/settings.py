9dc5473eb302f84a0fa27912ccf40d5638c51db0
Nacho Núñez <99191235+Pingu9191@users.noreply.github.com>
2024-03-08 10:07:39 +0100

    Update settings.py

--- persona/persona/settings.py
+++ persona/persona/settings.py
@@ -90,11 +90,25 @@ WSGI_APPLICATION = 'persona.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Add these at the top of your settings.py
+from os import getenv
+from dotenv import load_dotenv
+
+print("Using neon server...")
+print("It could go a bit slower.")
+# Replace the DATABASES section of your settings.py with this
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': getenv('PGDATABASE'),
+    'USER': getenv('PGUSER'),
+    'PASSWORD': getenv('PGPASSWORD'),
+    'HOST': getenv('PGHOST'),
+    'PORT': getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
 }
 
 ALLOWED_HOSTS = ['*']  # Quiza sea algo peligroso, pero por ahora lo dejamos asi
